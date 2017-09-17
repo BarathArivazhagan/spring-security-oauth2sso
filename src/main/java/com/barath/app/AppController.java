0b31325fadbded1d9fbb5d846fb87b206ce1b7b6
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-09-17 22:22:55 +0530

    first commit

---
+++ src/main/java/com/barath/app/AppController.java
@@ -0,0 +1,46 @@
+package com.barath.app;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AppController {
+	
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
+    @RequestMapping("/home1")
+    public String greeting(Model model) {
+      
+        return "index";
+    }
+    
+    @RequestMapping("/home")
+    @ResponseBody
+    public final String home() {
+        final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        logger.info(username);
+        return "Welcome, " + username;
+    }
+  
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
+    }
+}
