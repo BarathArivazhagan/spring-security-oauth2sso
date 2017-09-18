06a4e909b89a54d0b33e3002ca2084765e6f6888
Barath Arivazhagan <barath.arivazhagan@accenture.com>
2017-09-18 10:15:24 +0530

    code commit

---
+++ src/main/java/com/barath/app/SpringSecurityOauth2ssoApplication.java
@@ -0,0 +1,12 @@
+package com.barath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2ssoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth2ssoApplication.class, args);
+	}
+}
